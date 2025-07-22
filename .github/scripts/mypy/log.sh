6b2321263ba7752e66f43f3265fd52deeec8adeb
Rodrigo Muñoz <rod@rodmk.com>
2025-07-21 20:32:03 -0700

    error

--- .github/scripts/mypy/log.sh
+++ .github/scripts/mypy/log.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while read -r line; do
-  if [[ "$line" =~ ^([^:]+):([0-9]+):\ (\w+):\ (.*)$ ]]; then
+  if [[ "$line" =~ ^([^:]+):([0-9]+):[[:space:]]([a-z]+):[[:space:]](.*)$ ]]; then
     loglevel="notice"
     [[ "${BASH_REMATCH[3]}" == "error" ]] && loglevel="error"
     echo "::${loglevel} file=${BASH_REMATCH[1]},line=${BASH_REMATCH[2]}::${BASH_REMATCH[4]}"
